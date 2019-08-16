b855652cdb1c50c2e670bdaad0c3d1384af19047
McKay Waters <mckaypaswaters@gmail.com>
2019-08-16 17:26:40 -0600

    Finished nesting problem 1

--- nesting.js
+++ nesting.js
@@ -51,6 +51,17 @@ var employees = [
 */
 
 //Code Here
+function employeeUpdater(){
+  // for (let i = employees.length - 1; i >= 0; i--){
+   for (let i = 0; i < employees.length; i++) {
+    if (employees[i].firstName === 'Theo'){
+      employees.splice(i, 1)
+    } else if (employees[i].firstName === 'Lorie') {
+      employees[i].department = 'HR'
+    }
+  } return employees
+}
+console.log(employeeUpdater())
 
 
 
